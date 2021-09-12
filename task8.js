2ed4a549b5d823ed171201420ad4f4bb00dd62cb
katleho <katlehomofokeng@protonmail.com>
2021-09-12 15:14:45 +0200

    initial commit of task8.js file

---
+++ task8.js
@@ -0,0 +1,16 @@
+function numberToTime(number){
+    number % 60 == 1 ? minutes = 'minute' : minutes = 'minutes';
+
+    if (number < 60){
+        time = `${number} ${minutes}`
+    }
+    else{
+        number >= 120 ? hourly = 'hours' : hourly = 'hour';
+        number % 60  ? time = `${Math.floor(number/60)} ${hourly}, ${number%60} ${minutes}` : time = `${number/60} ${hourly}` ;
+    }
+    return time;
+
+
+}
+
+numberToTime(1);
